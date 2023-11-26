e2c5905a12b6bc55bbb31d310f9d8482c428ecd5
rsemenov <rsemenov@solanteq.com>
2023-11-26 22:33:26 +0300

    feat: add basic security config

---
+++ src/main/java/com/svi/group5/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.svi.group5.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/svi/group5/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.svi.group5.dao;
+
+import com.svi.group5.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
